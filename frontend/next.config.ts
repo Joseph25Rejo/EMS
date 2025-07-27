2e50778efa8576299e88698caaa03ffb2d03c5d2
Joseph25Rejo <joseph25rejo@gmail.com>
2025-07-27 23:05:26 +0530

    Hosting frontend on Vercel

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,12 +1,11 @@
 const nextConfig = {
-  output: 'export',
   async rewrites() {
     return [
       {
         source: '/api/:path*',
         destination: `${process.env.API_BASE_URL}/api/:path*`
       }
-    ]
+    ];
   },
   eslint: {
     ignoreDuringBuilds: true
